99a1fcaf71fbba443c408428faf1fcb221df4c54
OmercanYalcinn <yalcin.omercann@gmail.com>
2024-11-12 19:22:18 +0300

    ScriptUpdate 03
    The update for the spawn of platform prefab. The work is done on X axis.

--- Assets/_Scripts/PlatfromSpawner.cs
+++ Assets/_Scripts/PlatfromSpawner.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class PlatfromSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] platformPrefabs;  // array to hold platform types
-    public Transform playerTransform;   // Reference ro player position
+    public Transform platformPrefabTransform;   // Reference to player position
     [SerializeField] private Vector3 nextSpawnPoint;    // Position to spawn the next platform
     [SerializeField] private float platformLength = 25;     //The ground is (25,1,5)
 
     void Start(){
-        nextSpawnPoint = transform.position;
+        nextSpawnPoint = Vector3.zero;    // Vector3.zero form
     }
 
     void OnTriggerEnter(Collider other){
@@ -22,12 +22,11 @@ public class PlatfromSpawner : MonoBehaviour
     private void SpawnPoint(){
         // To select randomly one of the platform prefabs
         int platformIndex = Random.Range(0, platformPrefabs.Length); // For now with the two types of platforms, 1/2
-        GameObject newPlatform = Instantiate(platformPrefabs[platformIndex], nextSpawnPoint, Quaternion.identity);
+        
+        //now update the next spawn point, negative is working because the camera angle at the beggining is act like that way LOL 
+        nextSpawnPoint.x -= platformLength;
+        Instantiate(platformPrefabs[platformIndex], nextSpawnPoint, Quaternion.identity);
 
-        //now update the next spawn point
-        nextSpawnPoint.x += platformLength;
         Destroy(transform.parent.gameObject, 5f);
-
     }
-
 }
